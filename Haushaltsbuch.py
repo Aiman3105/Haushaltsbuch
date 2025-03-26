842e0314f9759fe2fd93a444f10b33fd0a4d717d
Aiman <aimanelkhamis@yahoo.de>
2025-03-26 23:17:42 +0100

    Zweite Seite mit Spaltn

--- Haushaltsbuch.py
+++ Haushaltsbuch.py
@@ -4,25 +4,25 @@ import Funktionen
 
 st.title("Haushaltsbuch")
 
-def add_Punkte_Natalie():
-    if st.session_state["neue_Punkte"]:
+def add_Natalie():
+    if st.session_state["neue_Punkte_N"]:
         alt=Funktionen.get_Punkte("Natalie")
-        dazu=int(st.session_state["neue_Punkte"])
+        dazu=int(st.session_state["neue_Punkte_N"])
         neu=alt+dazu
         Funktionen.update_Punkte("Natalie", neu)
-        st.session_state["neue_Punkte"]=""
+        st.session_state["neue_Punkte_N"]=""
 
 
 col_N, col_NP, col_A, col_AP=st.columns(4)
 with col_N:
     st.markdown("**Natalie**")
     st.text_input(label="", placeholder="Aufgabe",
-                  key="neue_Aufgabe")
+                  key="neue_Aufgabe_N")
 
 with col_NP:
     st.text(Funktionen.get_Punkte("Natalie"))
     st.text_input("", placeholder="Punkte",
-                  on_change=add_Punkte_Natalie, key="neue_Punkte")
+                  on_change=add_Natalie, key="neue_Punkte_N")
 
 with col_A:
     st.markdown("**Aiman**")
