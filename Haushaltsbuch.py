fc344fb6ef9b14a638daffb1bfb2900eb45a9032
Aiman <aimanelkhamis@yahoo.de>
2025-03-27 00:12:24 +0100

    Refresh Funktion

--- Haushaltsbuch.py
+++ Haushaltsbuch.py
@@ -4,6 +4,15 @@ import Funktionen
 
 st.title("Haushaltsbuch")
 
+col_N, col_NP, col_A, col_AP=st.columns(4)
+
+with open("Punkte_N.txt", "r") as file:
+    Punkte_N = file.readlines()
+
+with open("Aufgaben_N.txt", "r") as file:
+    Aufgaben_N = file.readlines()
+
+
 def add_Natalie():
     if st.session_state["neue_Punkte_N"]:
         alt=Funktionen.get_Punkte("Natalie")
@@ -12,46 +21,37 @@ def add_Natalie():
         Funktionen.update_Punkte("Natalie", neu)
 
         with col_NP:
-            with open("Punkte_N.txt", "r") as file:
-                Punkte_N = file.readlines()
             Punkte_N.append(st.session_state["neue_Punkte_N"]+"\n")
             with open("Punkte_N.txt", "w") as file:
                 file.writelines(Punkte_N)
         st.session_state["neue_Punkte_N"]=""
 
-        for Punkte in Punkte_N:
-            with col_NP:
-                st.text(Punkte)
-
-
-
     if st.session_state["neue_Aufgabe_N"]:
         with col_N:
-            with open("Aufgaben_N.txt", "r") as file:
-                Aufgaben_N = file.readlines()
             Aufgaben_N.append(st.session_state["neue_Aufgabe_N"]+"\n")
             with open("Aufgaben_N.txt", "w") as file:
                 file.writelines(Aufgaben_N)
         st.session_state["neue_Aufgabe_N"]=""
 
-        for Aufgabe in Aufgaben_N:
-            with col_N:
-                st.text(Aufgabe)
 
 
 
-
-col_N, col_NP, col_A, col_AP=st.columns(4)
 with col_N:
     st.markdown("**Natalie**")
     st.text_input(label="", placeholder="Aufgabe",
                   on_change=add_Natalie,
                   key="neue_Aufgabe_N")
 
+    for Aufgabe in Aufgaben_N:
+        st.text(Aufgabe)
+
 with col_NP:
     st.text(Funktionen.get_Punkte("Natalie"))
     st.text_input("", placeholder="Punkte",
                   on_change=add_Natalie, key="neue_Punkte_N")
+
+    for Punkte in Punkte_N:
+        st.text(Punkte)
 
 with col_A:
     st.markdown("**Aiman**")
@@ -60,6 +60,19 @@ with col_A:
 with col_AP:
     st.text(Funktionen.get_Punkte("Aiman"))
     st.text_input("", key="A_Punkte")
+
+
+if "spalte_geloescht" not in st.session_state:
+    st.session_state.spalte_geloescht = False
+if st.button("refresh"):
+    st.session_state.spalte_geloescht = True
+    with open("Aufgaben_N.txt", "w") as file:
+        Aufgaben_N=[]
+        file.writelines(Aufgaben_N)
+    with open("Punkte_N.txt", "w") as file:
+        Punkte_N=[]
+        file.writelines(Punkte_N)
+
 
 
 st.sidebar.page_link("pages/Übersicht.py", label="Zur Übersicht ➡️")
